d5f412ae11e52dc80f98182be58076df59ca9141
Djenane Hernández Rodríguez <djenanehernandezrodriguez@MacBook-Pro-de-Djenane.local>
2020-06-05 21:23:43 -0600

    other

---
+++ lab0910/app/src/main/java/com/example/lab0910/curso/addCurso.java
@@ -0,0 +1,15 @@
+package com.example.lab0910.curso;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.lab0910.R;
+import android.os.Bundle;
+
+public class addCurso extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_curso);
+
+    }
+}
