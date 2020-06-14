38648750d7149ac3bf84249d6a5a3e346469b9e6
Djenane Hernández Rodríguez <djenanehernandezrodriguez@MacBook-Pro-de-Djenane.local>
2020-06-13 23:57:24 -0600

    otro

--- lab0910/app/src/main/java/com/example/lab0910/principalMenu.java
+++ lab0910/app/src/main/java/com/example/lab0910/principalMenu.java
@@ -39,9 +39,7 @@ public class principalMenu extends AppCompatActivity implements NavigationView.O
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View view) {logOut();
             }
         });
 
@@ -145,5 +143,8 @@ public class principalMenu extends AppCompatActivity implements NavigationView.O
     }
 
     private void logOut() {
+        finish();
+        Intent a = new Intent(this, MainActivity.class);
+        startActivity(a);
     }
 }
